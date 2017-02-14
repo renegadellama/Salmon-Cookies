be118b35979c8c6f3c97731b239a6abcbbddd649
Isak Swearingen <isakswearingen@gmail.com>
2017-02-13 18:16:57 -0800

    all lists displayed, cap hill not working, not totals

--- salesapp.js
+++ salesapp.js
@@ -1,8 +1,7 @@
 'use strict';
-var storeHours = ['6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM', '8PM'];
 
 //price arrays
-
+var storeHours = ['6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM', '8PM'];
 var arrayPike = [];
 var arraySeatac = [];
 var arraySeattleCntr = [];
@@ -10,7 +9,7 @@ var arrayCapHill = [];
 var arrayAlki = [];
 
 //Store Objects
-
+//PIKE OBJECT
 var storeFirstPike = {
   minHourCustomers:23,
   maxHourCustomers:65,
@@ -22,9 +21,11 @@ var storeFirstPike = {
     for(var i = 0; i < 15; i++){
       arrayPike.push(Math.floor(this.avgCustomers() * this.avgCookiesPerCustomer));
     }
-    return pikeStore;
+    return arrayPike;
   }
 };
+
+//SEATAC object
 
 var storeSeatac = {
   minHourCustomers:3,
@@ -37,9 +38,11 @@ var storeSeatac = {
     for(var i = 0; i < 15; i++){
       arraySeatac.push(Math.floor(this.avgCustomers() * this.avgCookiesPerCustomer));
     }
-    return pikeStore;
+    return arraySeatac;
   }
 };
+
+//SEATTLE CENTER
 
 var storeSeattleCntr = {
   minHourCustomers: 11,
@@ -52,9 +55,11 @@ var storeSeattleCntr = {
     for(var i = 0; i < 15; i++){
       arraySeattleCntr.push(Math.floor(this.avgCustomers() * this.avgCookiesPerCustomer));
     }
-    return pikeStore;
+    return arraySeattleCntr;
   }
 };
+
+//CAPITOL HILL object
 
 var storeCapHill = {
   minHourCustomers:20,
@@ -67,9 +72,11 @@ var storeCapHill = {
     for(var i = 0; i < 15; i++){
       arraySeattleCntr.push(Math.floor(this.avgCustomers() * this.avgCookiesPerCustomer));
     }
-    return pikeStore;
+    return arrayCapHill;
   }
 };
+
+//ALKI object
 
 var storeAlki = {
   minHourCustomers: 2,
@@ -82,11 +89,74 @@ var storeAlki = {
     for(var i = 0; i < 15; i++){
       arrayAlki.push(Math.floor(this.avgCustomers() * this.avgCookiesPerCustomer));
     }
-    return pikeStore;
+    return storeAlki;
   }
 };
+//Pike
+var displayPike = function () {
+  var list = document.getElementById('pike');
+  for(var i = 0; i < arrayPike.length; i++) {
+    var item = document.createElement('li');
+    item.textContent = storeHours[i] + ': ' + arrayPike[i] + ' cookies.';
+    list.appendChild(item);
+  }
+  return list;
+};
+storeFirstPike.avgSales();
+displayPike();
 
-// var a = [];
-// for(var i=0; i<15; i++){
-//   a.push(Math.floor(Math.random() *  100));
-// };
+//Seatac
+
+var displaySeatac = function () {
+  var list = document.getElementById('seatac');
+  for(var i = 0; i < arraySeatac.length; i++) {
+    var item = document.createElement('li');
+    item.textContent = storeHours[i] + ': ' + arraySeatac[i] + ' cookies.';
+    list.appendChild(item);
+  }
+  return list;
+};
+storeSeatac.avgSales();
+displaySeatac();
+
+//SEATTLE CENTER
+
+var displaySeaCenter = function () {
+  var list = document.getElementById('seacenter');
+  for(var i = 0; i < arraySeattleCntr.length; i++) {
+    var item = document.createElement('li');
+    item.textContent = storeHours[i] + ': ' + arraySeattleCntr[i] + ' cookies.';
+    list.appendChild(item);
+  }
+  return list;
+};
+storeSeattleCntr.avgSales();
+displaySeaCenter();
+
+//CAPITOL HILL
+
+var displayCapHill = function () {
+  var list = document.getElementById('caphill');
+  for(var i = 0; i < arrayCapHill.length; i++) {
+    var item = document.createElement('li');
+    item.textContent = storeHours[i] + ': ' + arrayCapHill[i] + ' cookies.';
+    list.appendChild(item);
+  }
+  return list;
+};
+storeCapHill.avgSales();
+displayCapHill();
+
+//Alki
+
+var displayAlki = function () {
+  var list = document.getElementById('alkai');
+  for(var i = 0; i < arrayAlki.length; i++) {
+    var item = document.createElement('li');
+    item.textContent = storeHours[i] + ': ' + arrayAlki[i] + ' cookies.';
+    list.appendChild(item);
+  }
+  return list;
+};
+storeAlki.avgSales();
+displayAlki();
